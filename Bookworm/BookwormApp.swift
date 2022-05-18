9900e58fb7e13f97596520adfa223ea0ebc3fcb4
Isaac Kim <myong328@gmail.com>
2022-05-18 09:45:52 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Isaac Kim on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
