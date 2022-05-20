98c22382df7e0ea99a6071ac06f46c67617b7c35
Isaac Kim <myong328@gmail.com>
2022-05-20 09:51:21 -0700

    emojiView has been created

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,9 +9,48 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
   
     var body: some View {
-        Text("hello")
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        Text(book.title ?? "Unknown TRitle")
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+            
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                            print(showingAddScreen)
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
     }
 }
 
---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Isaac Kim on 5/20/22.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("🙈")
+        case 2:
+            return Text("😞")
+        case 3:
+            return Text("🙂")
+        case 4:
+            return Text("😋")
+        default:
+            return Text("🤩")
+            
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
+
